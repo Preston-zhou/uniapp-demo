348ff843c4dd368d78860444b2a38c4893764596
zhoupeilai <zhoupeilai001@163.com>
2020-09-09 09:04:51 +0800

    first commit

---
+++ uniapp-demo/pages/demo/demo.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<view class="box">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.box {
+	.demo {
+		color: $uni-color-primary;
+	}
+}
+</style>
---
+++ uniapp-demo/pages/details/details.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<button @click="handleClick(20, $event)" type="primary">button</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				showText: true
+			}
+		},
+		methods: {
+			handleClick(num,e){
+				console.log('aaa',num, e)
+			}
+		}
+	}
+</script>
+
+<style>
+@import url("./a.css");
+.box{
+	width:100px;
+	height: 100px;
+	background:#007AFF;
+}
+.box-active{
+	background-color: #DD524D;
+}
+.iconfont {
+	font-size: 50px;
+}
+</style>
